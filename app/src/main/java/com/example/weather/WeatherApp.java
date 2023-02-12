a2f9c263da9e7086f58379e358c9c7e36a7ef036
Pengjie Zhang <pengjie@google.com>
2023-02-11 21:23:17 -0800

    Add @HiltAndroidApp to bootstrap application with Dependency Injection

---
+++ app/src/main/java/com/example/weather/WeatherApp.java
@@ -0,0 +1,12 @@
+package com.example.weather;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+/**
+ * Hilt app entry point to bootstrap application.
+ */
+@HiltAndroidApp
+public class WeatherApp extends Application {
+}
